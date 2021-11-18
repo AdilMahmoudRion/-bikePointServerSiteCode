13162a560e2707e494dcc6966edb37b3830b4b2b
Adil_Mahmoud Rion <adilmahmoudrion@gamil.com>
2021-11-18 23:11:26 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,112 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.or2vp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("connect to the data base");
+    const database = client.db("bike-collection");
+    const bikeDetails = database.collection("bikes");
+    const bookingCollection = database.collection("booking");
+    const usersCollection = database.collection("users");
+
+// heroku deploy
+    app.get("/bikes", async (req, res) => {
+      const cursor = bikeDetails.find({});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+
+    app.get("/bookingPage/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await bikeDetails.findOne(query);
+      // console.log(service);
+      res.json(service);
+    });
+
+    app.get("/booking", async (req, res) => {
+      const email = req.query.email;
+      // const date = req.query.date;
+      const query = { email: email };
+      // console.log(query);
+      const cursor = bookingCollection.find(query);
+      const booking = await cursor.toArray();
+      res.json(booking);
+    });
+
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      const result = await bookingCollection.insertOne(booking);
+      res.json(result);
+    });
+
+    // app.get("/users/:email", async (req, res) => {
+    //   const email = req.params.email;
+    //   const query = { email: email };
+    //   const user = await usersCollection.findOne(query);
+    //   let isAdmin = false;
+    //   if (user?.role === "admin") {
+    //     isAdmin = true;
+    //   }
+    //   res.json({ admin: isAdmin });
+    // });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      console.log(result);
+      res.json(result);
+    });
+
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+
+    app.put("/users/admin", async (req, res) => {
+      const user = req.body;
+     
+          const filter = { email: user.email };
+          const updateDoc = { $set: { role: "admin" } };
+          const result = await usersCollection.updateOne(filter, updateDoc);
+          res.json(result);
+     
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello Doctors portal!");
+});
+
+app.listen(port, () => {
+  console.log(`listening at adil dot ${port}`);
+});
