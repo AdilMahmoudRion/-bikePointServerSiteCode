956aa25e8ad99b9d77b796232283b0c74ab29db8
Adil_Mahmoud Rion <adilmahmoudrion@gamil.com>
2021-11-19 04:13:06 +0600

    new deshboard

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient } = require("mongodb");
+const ObjectId  = require('mongodb').ObjectId;
 
 const port = process.env.PORT || 5000;
 
@@ -33,13 +34,13 @@ async function run() {
     });
 
 
-    // app.get("/bookingPage/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: ObjectId(id) };
-    //   const service = await bikeDetails.findOne(query);
-    //   // console.log(service);
-    //   res.json(service);
-    // });
+    app.get("/bookingPage/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await bikeDetails.findOne(query);
+      // console.log(service);
+      res.json(service);
+    });
 
     app.get("/booking", async (req, res) => {
       const email = req.query.email;
@@ -57,16 +58,16 @@ async function run() {
       res.json(result);
     });
 
-    // app.get("/users/:email", async (req, res) => {
-    //   const email = req.params.email;
-    //   const query = { email: email };
-    //   const user = await usersCollection.findOne(query);
-    //   let isAdmin = false;
-    //   if (user?.role === "admin") {
-    //     isAdmin = true;
-    //   }
-    //   res.json({ admin: isAdmin });
-    // });
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === "admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
 
     app.post("/users", async (req, res) => {
       const user = req.body;
